0c7dfe494797f0b4149857fdc3584eea6a90bf8f
unknown <plaskacz.panda@gmail.com>
2022-01-26 22:09:49 +0100

    Numpy problems

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-autodoc_mock_imports = ['more_itertools', 'shapely', 'movingpandas', 'haversine', 'skfuzzy', 'haversine', 'matplotlib', 'scipy', 'numpy', 'numba']
+autodoc_mock_imports = ['more_itertools', 'shapely', 'movingpandas', 'matplotlib', 'scikit-mobility', 'haversine', 'skfuzzy', 'haversine', 'matplotlib', 'scipy', 'numba']
 
 
 # -- Project information -----------------------------------------------------
